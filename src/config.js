562765f48ca98e3acb963ef712160975a146b54f
Yunseon Choi <yunseon.choi.1012@gmail.com>
2018-12-16 08:11:21 +0800

    First commit

---
+++ src/config.js
@@ -0,0 +1,9 @@
+/** swap out */
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/86fd7a70-6375-49c5-bea1-537cafdc372a/token"
+
+/** swap out */
+const instanceLocator = "v1:us1:86fd7a70-6375-49c5-bea1-537cafdc372a"
+
+export { tokenUrl, instanceLocator }
+
+
